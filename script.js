ebfec5f8881588081efbb735f3ae243c08566fca
Lalit Patil <147392059+Lalit1702@users.noreply.github.com>
2024-08-26 20:38:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function updateWindowSize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const sizeInfo = document.getElementById('sizeInfo');
+    sizeInfo.innerHTML = `<h1>Width: ${width} and Height: ${height}</h1>`;
+}
+
+// Call the function once to set the initial size
+updateWindowSize();
+
+// Attach the updateWindowSize function to the window's resize event
+window.addEventListener('resize', updateWindowSize);
